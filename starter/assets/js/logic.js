89de925b280dc9de4c5da1508e3d3fa9f1b63f75
andrewstewart1993@gmail.com <andystewart@Andrews-MacBook-Air.local>
2023-01-15 18:24:29 +0000

    starting query selectors added

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,6 @@
+//Variables to use throughout logic
+var startButton = document.querySelector("#start");
+
+//I want to put my start button in this section
+// Attach event listener to start button to call startGame function on click
+startButton.addEventListener("start-button", start);
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,9 @@
+// In this sectionI will be using a query selector to isolate the question found in the HTML
+var question = document.querySelector("#question")
+var choices = document.querySelectorAll(".choice-option")
+
+document.querySelector("#choices")
+document.querySelector("#choices")
+document.querySelector("#choices")
+document.querySelector("#choices")
+
