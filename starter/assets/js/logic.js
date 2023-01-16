512cdf51ca96c4711267bbcfa0261081bb724e28
andrewstewart1993@gmail.com <andystewart@Andrews-MacBook-Air.local>
2023-01-16 20:39:36 +0000

    More logic variables and start button work

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,6 +1,18 @@
 //Variables to use throughout logic
+//These will help intialise key parts of the quiz
+var score = 0
+var timeLeft = 75
+var timeInterval
 var startButton = document.querySelector("#start");
 
-//I want to put my start button in this section
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("start-button", start);
+
+//starting the quiz
+function startQuiz()
+
+var sScreen = document.getElementById("start-screen");
+
+start.Screen.classList.add("hide");
+
+//run the quiz and show the questions
+var qScreen = document.getElementByID("questions");
+qScreen.classList.remove("hide");
